328948b9afa93f3451af15a7755ef82c6448b639
liyachuan <liyachuan@rockontrol.com>
2013-12-16 23:24:11 +0800

    create demo/feedback.html

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -20,7 +19,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'demo/')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -28,8 +27,7 @@ if ('development' == app.get('env')) {
   app.locals.pretty = true;
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/feedback', routes.feedback);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

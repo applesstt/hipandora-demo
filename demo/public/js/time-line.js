1ac01868bf43eff34b703cb207c583890551b44f
liyachuan <liyachuan@rockontrol.com>
2014-01-15 16:50:29 +0800

    set timeline drag left and drag right function

--- demo/public/js/time-line.js
+++ demo/public/js/time-line.js
@@ -1,11 +1,33 @@
 var timeLine = (function() {
+  var _firstDay = 0;
+  var _days = 6;
 
   //设置当前天
   var _setCurDay = function() {};
+  var _resetDragBtn = function() {
+    $('.drag-left').css('visibility', (_firstDay == 0 ? 'hidden' : 'visible'));
+    $('.drag-right').css('visibility', (_firstDay + 5 == _days ? 'hidden' : 'visible'));
+  };
   //左侧按钮事件
-  var _dragLeft = function() {};
+  var _dragLeft = function() {
+    if(_firstDay > 0) {
+      $('.time-line-inner').animate({
+        left: parseFloat($('.time-line-inner').css('left')) + 160
+      });
+      _firstDay--;
+      _resetDragBtn();
+    }
+  };
   //右侧按钮事件
-  var _dragRight = function() {};
+  var _dragRight = function() {
+    if(_firstDay + 5 < _days) {
+      $('.time-line-inner').animate({
+        left: parseFloat($('.time-line-inner').css('left')) - 160
+      });
+      _firstDay++;
+      _resetDragBtn();
+    }
+  };
   //插入一天
   var _insertDay = function() {};
   //删除一天
@@ -19,7 +41,15 @@ var timeLine = (function() {
   var setCityName = function() {};
 
   //时间轴初始化
-  var init = function() {};
+  var init = function() {
+    $('.drag-left').click(function() {
+      _dragLeft();
+    });
+    $('.drag-right').click(function() {
+      _dragRight();
+    });
+    _resetDragBtn();
+  };
   return {
     init: init,
     setCityName: setCityName,
